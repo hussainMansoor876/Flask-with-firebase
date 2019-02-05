0df53ab1d72aa20c01913b504d77a9c8b81637f8
Mansoor Hussain <hussainmansoor876@gmail.com>
2019-02-05 23:15:36 +0500

    Flask with firebase Authentication

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ config = {
 
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
+auth = firebase.auth()
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -24,19 +25,26 @@ def index():
             <h2>User</h2>
             <input type="text" name="user">
             <h2>Email</h2>        
-            <input type="email" name="email">        
+            <input type="email" name="email"> 
+            <h2>Password</h2>        
+            <input type="password" name="pass">       
             <input type="submit" value="Submit">
             </form>
         '''
     else:
         data = {'user': request.form['user'],'email': request.form['email']}
         db.child('Users').push(data)
+        # user = auth.create_user_with_email_and_password(request.form['email'],request.form['pass'])
+        user1 = auth.sign_in_with_email_and_password(request.form['email'],request.form['pass'])
+        # print(user1)
         return '''
             <form method="post">
             <h2>User</h2>
             <input type="text" name="user">
             <h2>Email</h2>        
-            <input type="email" name="email">        
+            <input type="email" name="email">
+            <h2>Password</h2>        
+            <input type="password" name="pass">         
             <input type="submit" value="Submit">
             </form>
         '''
